24ef44f0afe7c346a95df1d7a7266abf14a4fe1a
Egor Druzhinin <druzhinin.egor@gmail.com>
2020-12-06 17:50:30 +0300

    fix scripts

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 cd site/
 git clone git@gl.appris.by:rating_mak/chgk_rating.git .
+cp .env.example .env
+mkdir var/cache/dev/
+sudo chmod -R 777 var/
+
 cd ..
 cd api/
 git clone git@gl.appris.by:rating_mak/api.git .
+cp .env.example .env
+mkdir var/cache/dev/
+sudo chmod -R 777 var/
